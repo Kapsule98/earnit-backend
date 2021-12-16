097f70dd0be01c3d0c2fb09ea4d26c8f8e76a480
Kapsule98 <kapil9787@gmail.com>
2021-12-16 21:16:04 +0530

    modified offer structure in offers by city

--- main/service/category_service.py
+++ main/service/category_service.py
@@ -192,7 +192,7 @@ class CategoryService:
         for offer in offers:
             shop_id = offer['shop_id']
             shop = seller_table.find_one({'username':shop_id})
-            shop_city = shop['city']
+            seller = seller_table.find_one({'username':offer['shop_id']})
             if shop['city'] in cities:
                 obj = {
                     "validity":offer['validity'],
@@ -207,6 +207,10 @@ class CategoryService:
                     'offer_price':offer['offer_price'],
                     'bio':offer['bio'],
                     'image_url':offer['image_url'],
+                    'shop_name':seller['shop_name'],
+                    'category':seller['category'],
+                    'seller_display_name':seller['display_name'],
+                    'seller_email':seller['email'],
                 }
                 res.append(obj)
         if res is None or len(res) == 0:
@@ -219,6 +223,6 @@ class CategoryService:
                 "msg":"Offers fetched in cities",
                 "status":200,
                 "cities":cities,
-                "offers":res
+                "active_offers":res
             })
         
